94cc52d208b1fcd597e9a19f1c5e53f679c50a70
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2022-12-06 20:12:47 -0600

    V-1.0.0

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,6 @@
 import bcrypt from 'bcrypt';
+import {fileURLToPath} from 'url';
+import { dirname } from 'path';
 
 export const createHash = async(password) =>{
     const salts = await bcrypt.genSalt(10);
@@ -6,3 +8,8 @@ export const createHash = async(password) =>{
 }
 
 export const passwordValidation = async(user,password) => bcrypt.compare(password,user.password);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default __dirname;
