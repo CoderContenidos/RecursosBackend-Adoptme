4df363c6ddbe28850bcdf8fb094c694a60840e56
AllanNara <allannara@outlook.com>
2024-03-11 18:35:07 -0300

    add swagger yaml

--- src/app.js
+++ src/app.js
@@ -1,27 +1,41 @@
+// import listEndpoints from "express-list-endpoints"
+import cookieParser from "cookie-parser";
+import displayRoutes from "express-routemap";
 import express from "express";
 import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
+import path from "path";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express"
 
-import displayRoutes from "express-routemap";
-// import listEndpoints from "express-list-endpoints"
-
-import usersRouter from "./routes/users.router.js";
-import petsRouter from "./routes/pets.router.js";
 import adoptionsRouter from "./routes/adoption.router.js";
+import petsRouter from "./routes/pets.router.js";
 import sessionsRouter from "./routes/sessions.router.js";
+import usersRouter from "./routes/users.router.js";
+import __dirname from "./utils/index.js";
 
-const app = express();
 const PORT = process.env.PORT || 8080;
 const URI = `mongodb://127.0.0.1:27017/adoptme`;
-mongoose.connect(URI);
+const app = express();
+const specs = swaggerJSDoc({
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Adoptme Documentation",
+      description: "API de prueba para clase de Swagger",
+    }
+  },
+  apis: [path.join(__dirname, "../docs/**/*.yaml")]
+});
 
+mongoose.connect(URI);
 app.use(express.json());
 app.use(cookieParser());
-
+app.use("/apidocs", swaggerUi.serve, swaggerUi.setup(specs))
 app.use("/api/users", usersRouter);
 app.use("/api/pets", petsRouter);
 app.use("/api/adoptions", adoptionsRouter);
 app.use("/api/sessions", sessionsRouter);
+
 
 mongoose.connection.on("connected", () => {
 	console.log("Mongo Database connected");
